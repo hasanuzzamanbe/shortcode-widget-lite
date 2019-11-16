72119c2520c4f7b05e529da3ddf316148558f3e9
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2019-11-16 19:07:32 +0600

    widget created

---
+++ includes/ShortcodeAttrWidget.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ShortcodeAttr\includes;
+
+class ShortcodeAttrWidget extends \WP_Widget
+{
+    function __construct()
+    {
+        parent::__construct(
+            'shortcode_widget_with_attribute',
+            esc_html__('Shortcode Widget Lite', 'shortcodewidgetlite'),
+            array('description' => esc_html__('Use shortcode on your widget', 'shortcodewidgetlite'),)
+        );
+    }
+
+    public function widget($args, $instance)
+    {
+        $shortcode = empty($instance['shortcode']) ? '' : $instance['shortcode'];
+        if (!$shortcode) {
+            return;
+        }
+        echo $args['before_widget'];
+        if (!empty($instance['title'])) {
+            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        }
+        if ($shortcode != '') {
+            echo do_shortcode($shortcode);
+        }
+        echo $args['after_widget'];
+    }
+
+    public function form($instance)
+    {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = __('', 'shortcodewidgetlite');
+        }
+        if (isset($instance['shortcode'])) {
+            $shortcode = $instance['shortcode'];
+        } else {
+            $shortcode = __('', 'shortcodewidgetlite');
+        }
+        // Widget admin form
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (optional):'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+                   name="<?php echo $this->get_field_name('title'); ?>" type="text"
+                   value="<?php echo esc_attr($title); ?>"/>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('shortcode'); ?>"><?php _e('Shortcode :'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('shortcode'); ?>"
+                   name="<?php echo $this->get_field_name('shortcode'); ?>" type="text"
+                   value="<?php echo esc_attr($shortcode); ?>"/>
+        </p>
+        <?php
+    }
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['shortcode'] = (!empty($new_instance['shortcode'])) ? strip_tags($new_instance['shortcode']) : '';
+        return $instance;
+    }
+}
---
+++ shortcode-widget-lite.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+Plugin Name: Shortcode Widget Lite
+Plugin URI:  https://hasanuzzaman.com/shortcodewidget
+Description: Show data from any shortcode in your widget.
+Version: 1.0.1
+Author: Hasanuzzaman
+Author URI: https://hasanuzzaman.com
+License: A "Slug" license name e.g. GPL2
+Text Domain: shortcodewidgetlite
+*/
+
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright 2019 WPManageNinja LLC. All rights reserved.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!defined('SHORTCODELITE_VERSION')) {
+    define('SHORTCODELITE', true);
+    define('SHORTCODELITE_VERSION', '1.0.0');
+    define('SHORTCODELITE_MAIN_FILE', __FILE__);
+    define('SHORTCODELITE_URL', plugin_dir_url(__FILE__));
+    define('SHORTCODELITE_DIR', plugin_dir_path(__FILE__));
+
+    class ShortcodeAttr
+    {
+        public function boot()
+        {
+            add_action('widgets_init', function ()
+            {
+                require_once(SHORTCODELITE_DIR . 'includes/ShortcodeAttrWidget.php');
+                register_widget('ShortcodeAttr\includes\ShortcodeAttrWidget');
+            });
+        }
+
+    }
+
+    add_action('plugins_loaded', function () {
+        (new ShortcodeAttr())->boot();
+    });
+
+} else {
+    add_action('admin_init', function () {
+        deactivate_plugins(plugin_basename(__FILE__));
+    });
+}
